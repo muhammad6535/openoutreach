b00cc69e46c1f8c241259a8e3cd2e909d980c40f
User <user@example.com>
2026-06-10 14:24:41 +0300

    fix: add CSRF_TRUSTED_ORIGINS for Railway domain

--- linkedin/django_settings.py
+++ linkedin/django_settings.py
@@ -13,6 +13,10 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "openoutreach-local-dev-key-cha
 DEBUG = os.environ.get("DJANGO_DEBUG", "True").lower() in ("true", "1", "yes")
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "*").split(",")
+CSRF_TRUSTED_ORIGINS = os.environ.get(
+    "DJANGO_CSRF_TRUSTED_ORIGINS",
+    "https://" + os.environ.get("RAILWAY_PUBLIC_DOMAIN", "localhost"),
+).split(",")
 
 INSTALLED_APPS = [
     "django.contrib.sites",
